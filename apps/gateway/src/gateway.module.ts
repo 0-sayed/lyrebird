ab2e685491fc485184f4db4d06697e9418b0fa8d
0-sayed <sayed.ashraf@inframodern.com>
2025-12-23 21:40:11 +0200

    feat(gateway): integrate RabbitMQ messaging

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -3,9 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
 import { DatabaseModule } from '@app/database/database.module';
+import { RabbitmqModule } from '@app/rabbitmq';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    RabbitmqModule,
+  ],
   controllers: [GatewayController],
   providers: [GatewayService],
 })
