6927cae4c4a2cbb481ea505e15fa42eb3c5a1f42
0-sayed <sayed.ashraf@inframodern.com>
2025-12-25 19:19:00 +0200

    chore(config): update configuration files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -30,9 +30,10 @@ export default tseslint.config(
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unsafe-call': 'warn',
       'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
