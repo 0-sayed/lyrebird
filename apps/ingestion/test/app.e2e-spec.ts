dc9afd17d0864175695bc98d6fed4a16166d5d15
0-sayed <sayed.ashraf@inframodern.com>
2026-01-07 08:30:13 +0200

    test(ingestion): remove PollingScraperService from IngestionController e2e tests

--- apps/ingestion/test/app.e2e-spec.ts
+++ apps/ingestion/test/app.e2e-spec.ts
@@ -42,7 +42,6 @@ describe('IngestionController (e2e)', () => {
       controllers: [IngestionController, HealthController],
       providers: [
         IngestionService,
-        PollingScraperService,
         {
           provide: DatabaseService,
           useValue: mockDatabaseService,
