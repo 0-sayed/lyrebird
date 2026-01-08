94dcc6e725f48e8251ae417b211a5acc77d2e133
0-sayed <sayed.ashraf@inframodern.com>
2026-01-08 08:34:05 +0200

    test(jest): increase timeout and add global setup

--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -2,3 +2,14 @@
 jest.mock('uuid', () => ({
   v4: jest.fn(() => '00000000-0000-0000-0000-000000000000'),
 }));
+
+// Increase Jest's default timeout for cleanup operations
+// Some NestJS internal operations may take longer to settle
+jest.setTimeout(10000);
+
+// Use beforeAll to set up any global state
+beforeAll(() => {
+  // Suppress console output during tests for cleaner output
+  // Comment this out if you need to debug test issues
+  // jest.spyOn(console, 'log').mockImplementation(() => {});
+});
