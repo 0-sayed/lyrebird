026c9d3cb11a647098c76ca89052c3a507f92bcf
0-sayed <sayed.ashraf@inframodern.com>
2026-01-28 12:52:14 +0200

    chore: remove unused index.ts barrel files
    Remove empty barrel files from analysis and ingestion apps.

--- apps/analysis/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './analysis.service';
-export * from './analysis.controller';
-export * from './analysis.module';
-export * from './services/bert-sentiment.service';
--- apps/ingestion/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './ingestion.service';
-export * from './ingestion.controller';
-export * from './ingestion.module';
-export * from './scrapers/polling-scraper.service';
