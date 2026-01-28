7f48b5b790ddbed222ae6aa3d3aa48d600d72a34
0-sayed <sayed.ashraf@inframodern.com>
2026-01-28 12:49:43 +0200

    feat(bluesky): export Jetstream services from bluesky module
    Register JetstreamClientService, DidResolverService, and
    CursorPersistenceService in BlueskyModule and export from index.

--- libs/bluesky/src/bluesky.module.ts
+++ libs/bluesky/src/bluesky.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { BlueskyClientService } from './bluesky-client.service';
+import { JetstreamClientService } from './jetstream-client.service';
+import { DidResolverService } from './did-resolver.service';
+import { CursorPersistenceService } from './cursor-persistence.service';
 
 @Module({
   imports: [ConfigModule],
-  providers: [BlueskyClientService],
-  exports: [BlueskyClientService],
+  providers: [
+    BlueskyClientService,
+    JetstreamClientService,
+    DidResolverService,
+    CursorPersistenceService,
+  ],
+  exports: [
+    BlueskyClientService,
+    JetstreamClientService,
+    DidResolverService,
+    CursorPersistenceService,
+  ],
 })
 export class BlueskyModule {}
--- libs/bluesky/src/index.ts
+++ libs/bluesky/src/index.ts
@@ -1,3 +1,7 @@
 export * from './bluesky.module';
 export * from './bluesky-client.service';
 export * from './types';
+export * from './jetstream-client.service';
+export * from './jetstream-types';
+export * from './did-resolver.service';
+export * from './cursor-persistence.service';
